16101531499cade5ea6a6e1df4aae7cbb951583b
sakti swarupa mallick <saktiswarupamallick@gmail.com>
2024-01-23 07:51:31 +0530

    commit
    3D portfolio website using Three.js and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+
+
+const App = () => {
+  return (
+    <h1 className="text-red-500">hello world</h1>
+  );
+};
+
+export default App;
